db4a89d22b20d4edb68626c8263f3ff0c8e79308
doncasanova <ethanjuan@yahoo.com>
2018-10-18 22:10:48 -0500

    testing multiple videos

--- js/video.js
+++ js/video.js
@@ -13,3 +13,29 @@ function myFunction() {
         btn.innerHTML = "Play";
     }
 }
+
+var video2 = document.getElementById("myVideo2");
+var btn2 = document.getElementById("myBtn2");
+
+function myFunction2() {
+    if (video2.paused) {
+        video2.play();
+        btn2.innerHTML = "Pause";
+    } else {
+        video2.pause();
+        btn2.innerHTML = "Play";
+    }
+}
+
+var video3 = document.getElementById("myVideo3");
+var btn3 = document.getElementById("myBtn3");
+
+function myFunction3() {
+    if (video3.paused) {
+        video3.play();
+        btn3.innerHTML = "Pause";
+    } else {
+        video3.pause();
+        btn3.innerHTML = "Play";
+    }
+}
