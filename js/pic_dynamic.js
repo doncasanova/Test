5b3cc7a84f1e19cce19e7b29d686f1365bb54ed6
Don Casanova <ethanjuan@yahoo.com>
2018-10-23 12:36:30 -0500

    working on image dynamic load

--- js/pic_dynamic.js
+++ js/pic_dynamic.js
@@ -1,13 +1,9 @@
-var image = { imageAddy: "./images/dixie on a bird.jpg" };
+var image = ["./images/dixie on a bird.jpg", "./images/dixie.jpg", "./images/food turkey 068.JPG", "./images/skiing.jpg", "./images/SnarlingWolf.jpg"];
 addImage();
 
 function addImage() {
-    console.log("inside addImage");
-    //$(".test").append("include/test.html");
-    $(".insertImagesHere").append(` <div class="container"><div class="row"><div class="col-3"><div class="content"><img src= "${image.imageAddy}" alt="Italian Trulli"> <div class="content"><h1>Heading</h1><p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p><button id="myBtn" onclick="myFunction()">Pause</button></div></div></div></div>`);
+    for (i = 0; i < image.length; i++) {
+    $(".insertImagesHere").append(`<div class="col-3 "><img class="imageStyle "src= "${image[i]}" alt="Italian Trulli"><h1>Heading</h1><p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p><button id="myBtn" onclick="myFunction()">Pause</button></div>`);
 }
 
-//$("#link").click(function () {
-//    $("#include").append("include/test.html");
-//});
-
+}
