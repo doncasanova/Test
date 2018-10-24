ff6d6ebcfc5edc5865627b40b3a230922502631e
Don Casanova <ethanjuan@yahoo.com>
2018-10-24 10:36:48 -0500

    removed col div and add center justify to row

--- js/pic_dynamic.js
+++ js/pic_dynamic.js
@@ -3,7 +3,7 @@ addImage();
 
 function addImage() {
     for (i = 0; i < image.length; i++) {
-    $(".insertImagesHere").append(`<div class="col-3 test"><img class="imageStyle imageHover "src= "${image[i]}" alt="Italian Trulli"><h1>Heading</h1><p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p><button id="myBtn" onclick="myFunction()">Pause</button></div>`);
+    $(".insertImagesHere").append(`<img class="imageStyle imageHover "src= "${image[i]}" alt="Italian Trulli">`);
 }
 
 }
