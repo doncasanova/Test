a98501add96c63192d4efe2878789c31dc3dcf61
Don Casanova <ethanjuan@yahoo.com>
2018-11-02 12:24:36 -0500

    testing mouse up down

--- js/pic_dynamic.js
+++ js/pic_dynamic.js
@@ -25,3 +25,19 @@ $(newLocal).on("click", function () {
 });
 
 
+$("#target").mouseup(function () {
+    alert("Handler for .mouseup() called.");
+});
+
+$("#other").click(function () {
+    $("#target").mouseup();
+});
+
+$("p")
+    .mouseup(function () {
+        $(this).append("<span style='color:#f00;'>Mouse up.</span>");
+    })
+    .mousedown(function () {
+        $(this).append("<span style='color:#00f;'>Mouse down.</span>");
+    });
+
